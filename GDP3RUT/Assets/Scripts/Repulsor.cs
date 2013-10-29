0d004556cc89c54b7fe8835c60778572fbe09705
RandomGuy928 <randomguy928@gmail.com>
2013-10-29 17:26:05 -0400

    Definitely not dividing by zero

--- GDP3RUT/Assets/Scripts/Repulsor.cs
+++ GDP3RUT/Assets/Scripts/Repulsor.cs
@@ -24,12 +24,15 @@ public class Repulsor : MonoBehaviour {
 	
 	public Vector3 Contribution(Vector3 point){
 		Vector3 vec;
+		if(range == 0)
+			return Vector3.zero;
 		if(isGravityPlane)
 			vec = gravityPlaneVector;
 		else
 			vec = Vector3.Normalize (point - transform.position);
 		
 		float dist = Vector3.Distance (point, transform.position);
+		
 		if(dist > range)
 			return Vector3.zero;
 		return strength * strengthCurve.Evaluate(dist/range) * vec;
