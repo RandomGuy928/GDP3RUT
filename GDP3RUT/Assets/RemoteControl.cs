cc8b42eddc6fe1c27b7fecf5f1dad2c822066887
Jakobok <jakobo123dk@yahoo.dk>
2013-10-30 16:01:51 -0400

    Remote tuner

---
+++ GDP3RUT/Assets/RemoteControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RemoteControl : MonoBehaviour {
+	
+	Repulsor[] controlledRepulsors = new Repulsor[3];
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		SelectRepulser()
+	}
+	
+	void SelectRepulser ()
+	{
+		// check if cone collides with repulsors
+		// if more than 1: select the repulsor that is closest to the center of the Cone.
+		//
+	}
+		
+	void AssignRepulsor ()	
+	{
+		
+		
+		
+		// Assign repulsor to the specified key. Add to array of repulsors. 
+		
+		
+	}
+}
