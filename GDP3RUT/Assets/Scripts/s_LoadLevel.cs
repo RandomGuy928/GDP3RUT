7c284d7fce6740bf121cd4aa64c582f69fd4447d
Jakobok <jakobo123dk@yahoo.dk>
2013-11-01 13:21:43 -0400

    End Script

---
+++ GDP3RUT/Assets/Scripts/s_LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class s_LoadLevel : MonoBehaviour {
+	// Array of levels
+	string[] levelArray = { "SimpleLevel", "ClimbLevel", "FlingLevel", "HarderLevel" };
+	// On triggerenter:
+	void OnTriggerEnter(Collider other) {
+		
+		String curLevel = Application.loadedLevelName;
+		for(int i=0; i<levelArray.Length; i++) 
+        {
+			if(levelArray[i].Equals(curLevel))
+			{
+				String nextLevel = levelArray[i+1];
+				Application.LoadLevel(nextLevel);	
+			}    
+        }
+	}
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+} 
