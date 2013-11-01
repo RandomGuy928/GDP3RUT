fd2a845eb43dbf37516e08adb4fceaa9f6274abb
Jakobok <jakobo123dk@yahoo.dk>
2013-11-01 16:25:29 -0400

    End working

--- GDP3RUT/Assets/Scripts/s_LoadLevel.cs
+++ GDP3RUT/Assets/Scripts/s_LoadLevel.cs
@@ -5,22 +5,28 @@ using System.Collections;
 public class s_LoadLevel : MonoBehaviour {
 	// Array of levels
 	string[] levelArray = { "SimpleLevel", "ClimbLevel", "FlingLevel", "HarderLevel" };
+	GameObject player;
 	// On triggerenter:
-	void OnTriggerEnter(Collider other) {
-		
-		String curLevel = Application.loadedLevelName;
-		for(int i=0; i<levelArray.Length; i++) 
-        {
-			if(levelArray[i].Equals(curLevel))
-			{
-				String nextLevel = levelArray[i+1];
-				Application.LoadLevel(nextLevel);	
-			}    
-        }
+	void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.Equals(player))
+		{
+			String curLevel = Application.loadedLevelName;
+			for(int i=0; i<levelArray.Length; i++) 
+	        {
+				if(levelArray[i].Equals(curLevel))
+				{
+					String nextLevel = levelArray[i+1];
+					
+					Application.LoadLevel(nextLevel);	
+				}    
+	        }
+		}
 	}
+	
+	
 	// Use this for initialization
 	void Start () 
 	{
-
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 } 
